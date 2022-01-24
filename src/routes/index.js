90d8e4b603aae3e1423115d6d51e6ec08e7cfda7
ismaelcleiton <ismaelcleitonsf@gmail.com>
2022-01-23 21:19:01 -0300

    initial commit

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import {
+  Route,
+  BrowserRouter,
+  Routes,
+} from 'react-router-dom';
+import Home from '../pages/home/'
+
+export default function AppRoutes() {
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={Home()} />
+      </Routes>
+    </BrowserRouter>
+  );
+
+}
